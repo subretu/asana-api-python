044575f3e9d219ef225d8cb17b0270f633315dbb
miyahara <50367145+subretu@users.noreply.github.com>
2022-01-25 21:51:58 +0900

    プロジェクトからタスク抽出処理を実装

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # asana-api-python
+
+## 概要
+- AsanaAPIのPythonラッパー。
+- 公式のAPIリファレンスは下記を参照。
+  - https://developers.asana.com/docs/asana
+## サンプル
+- プロジェクトからタスク一覧を抽出
+
+```python
+from asana import asana
+
+asana_api = asana.Asana("AsanaAPIKey")
+# 引数：プロジェクトID、パラメータを含んだパス
+task_data = asana_api.get_tasks_from_project(1234567890123456, "opt_fields=completed,name")
+```
+  
---
+++ asana/asana.py
@@ -0,0 +1,19 @@
+import requests
+
+
+class Asana:
+    def __init__(self, apikey):
+        self.asana_url = "https://app.asana.com/api"
+        self.api_version = "1.0"
+        self.aurl = "/".join([self.asana_url, self.api_version])
+        self.apikey = apikey
+
+    def get_tasks_from_project(self, project_id, api_target):
+        """
+        project_id : プロジェクトID
+        api_target : パラメータを含んだパス(詳細:https://developers.asana.com/docs/get-tasks-from-a-project)
+        """
+        url = self.aurl + f"/projects/{project_id}/tasks/?{api_target}"
+        req = requests.get(url, auth=(self.apikey, ""))
+        data = req.json()
+        return data
